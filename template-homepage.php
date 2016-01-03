2c5f046e3a2b4ea956aeee0b1349135ba5757e1b
hyzershop <danny@hyzershop.com>
2016-01-03 18:30:46 -0500

    dfsads

--- template-homepage.php
+++ template-homepage.php
@@ -44,10 +44,9 @@ get_header(); ?>
 						wp_reset_postdata();
 					endif; ?>
 				</div>
-				<div class="featured-disc-twitter">
-					<div class="twitter-feed">
-						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-					</div>
+				<div class="featured-disc-twitter twitter-feed">
+					<a class="twitter-timeline" href="https://twitter.com/search?q=%40discraftdg" data-widget-id="680632584016162816">Tweets about @discraftdg</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 			</section>
 
