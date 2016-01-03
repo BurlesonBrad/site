1f2bbe3059880c44afaf41d473efe7b7d63fe0dc
hyzershop <danny@hyzershop.com>
2016-01-03 16:14:59 -0500

    asdfadfs

--- template-homepage.php
+++ template-homepage.php
@@ -14,6 +14,12 @@
 
 get_header(); ?>
 
+	<style>
+		main > article > .entry-header {
+			display:none;
+		}
+	</style>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
