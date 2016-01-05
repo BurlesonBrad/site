70e2d5aad427c341e8759d489ac166487f9bb87a
hyzershop <danny@hyzershop.com>
2016-01-05 10:20:44 -0500

    custom masthead

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,18 @@
 
 $(document).ready(function() {
 	$("#sb_instagram").appendTo("#main");
-});
 
+	function customMasthead() {
+		var $utilNavWrap = $("<div class='nav-top'></div>");
+		var $utilNav = $utilNavWrap.wrapInner("<div class='col-full'></div>");
+
+		$utilNav.prependTo("#masthead");
+		$("header .secondary-navigation").prependTo($utilNav);
+		$("header .site-branding").prependTo($utilNav);
+	}
+	customMasthead();
+
+});
 
 
 })(jQuery);
