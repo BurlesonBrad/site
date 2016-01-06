ea01f5558d868884e6cba6226de55fec3d5c9442
hyzershop <danny@hyzershop.com>
2016-01-06 11:57:37 -0500

    sdfaasfd

--- template-homepage.php
+++ template-homepage.php
@@ -47,7 +47,7 @@ get_header(); ?>
 				</div>
 				<div class="featured-disc-twitter twitter-feed">
 					<div class="featured-disc-inner">
-						<h2>@<?php echo get_brands( $post_id ); ?></h2>
+						<h2><span>@</span><?php echo get_brands( $post_id ); ?></h2>
 						<a class="twitter-timeline" href="https://twitter.com/search?q=%40discraftdg" data-widget-id="680632584016162816">Tweets about @discraftdg</a>
 						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					</div>
