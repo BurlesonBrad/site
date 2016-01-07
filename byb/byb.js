e0b436893f60dc157406236f48d50e233b340cf1
hyzershop <danny@hyzershop.com>
2016-01-07 00:59:26 -0500

    sdfafsda

--- byb/byb.js
+++ byb/byb.js
@@ -7,7 +7,7 @@ function getBag(bc) {
 	if ( Cookies.get('byb') ) {
 		var the_bag = Cookies.getJSON('byb');
 		var discs = the_bag.bag.discs;
-		for (index = 0; index < discs.length; ++index) {
+		for (index = 1; index < discs.length; ++index) {
 		    bc.prepend("<div class='disc'><img src='/wp-content/uploads/" + discs[index]['slug'] + ".png' alt='" + discs[index]['name'] + "' /></div>");
 		}
 	} else {
@@ -80,7 +80,6 @@ if ( $("body").hasClass("single-product") ) {
 		addToBag( slug, type );
 	});
 }
-
 
 if ( $("body").hasClass("page-id-45") ) {
 	var $bag_container = $("#bag");
