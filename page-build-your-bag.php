629da8d306b66b9d679b282ee1843bdd76d6b848
hyzershop <danny@hyzershop.com>
2016-01-08 13:37:20 -0500

    dfsaafds

--- page-build-your-bag.php
+++ page-build-your-bag.php
@@ -37,8 +37,8 @@ get_header(); ?>
 					<?php
 					function getDiscBags() {
 						$byb_cookie = $_COOKIE['byb'];
-						$byb_json = '\"' . $byb_cookie . '\"';
-						$byb_cookie_obj = json_decode( $byb_json );
+						$byb_json = '"' . $byb_cookie . '"';
+						$byb_cookie_obj = json_decode( $byb_json, true );
 						echo $byb_cookie_obj;
 
 						if ( isset($byb_cookie) ) {
