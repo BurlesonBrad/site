5aeb5ab7407a8d0a6078cf6409188eed8cb35d7b
hyzershop <danny@hyzershop.com>
2016-01-07 23:53:08 -0500

    dfasdsf

--- page-build-your-bag.php
+++ page-build-your-bag.php
@@ -15,22 +15,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<!-- 			<?php while ( have_posts() ) : the_post(); ?>
+ 			<?php // while ( have_posts() ) : the_post(); ?>
 
 				<?php
-				do_action( 'storefront_page_before' );
+				// do_action( 'storefront_page_before' );
 				?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php // get_template_part( 'content', 'page' ); ?>
 
 				<?php
 				/**
 				 * @hooked storefront_display_comments - 10
 				 */
-				do_action( 'storefront_page_after' );
+				// do_action( 'storefront_page_after' );
 				?>
 
-			<?php endwhile; // end of the loop. ?> -->
+			<?php // endwhile; // end of the loop. ?>
 
 			<div id="byb-wrapper" data-temp-user="0">
 				<form id="byb-form">
