556f4fee2d86afb25a305f3a8616c72d6a576f48
hyzershop <danny@hyzershop.com>
2016-01-09 21:24:30 -0500

    bhkl

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -18,7 +18,7 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 ?>
 
-<script type="text/javascript">var $discSlug = <?php echo $post->post_name; ?>;</script>
+<script type="text/javascript">var $discSlug = "<?php echo $post->post_name; ?>";</script>
 
 <div class="product_meta">
 
