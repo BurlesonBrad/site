d9e4482a3c4394af677ab331c8d3d1ee254a854c
hyzershop <danny@hyzershop.com>
2016-01-10 00:10:17 -0500

    asf

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			do_action( 'woocommerce_review_order_before_cart_contents' );
 
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-				var_dump( $cart_item['data']->post->post_name );
+				var_dump( $cart_item['data'] );
 			}
 
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 					?>
-					<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+					<tr data-product-slug="<?php echo $cart_item['data']->post->post_name; ?>" class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 						<td class="product-name">
 							<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;'; ?>
 							<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?>
