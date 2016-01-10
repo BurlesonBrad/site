ce70ec74052cf037e63b0e0d04fba0ccaedb8ce3
hyzershop <danny@hyzershop.com>
2016-01-09 22:52:14 -0500

    adfsfsad

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -43,7 +43,7 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
 }
 
 $type = wp_get_post_terms( $post->ID, 'disc-type' );
-echo $type[0];
+var_dump( $type[0] );
 ?>
 <li data-product-slug="<?php echo $post->post_name; ?>" data-disc-type="<?php echo $type[0]; ?>" <?php post_class( $classes ); ?>>
 
