7bb7d58a430e9045bb186576266c83bb9ab0f2ee
hyzershop <danny@hyzershop.com>
2016-01-09 23:51:09 -0500

    sdfasdfa

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -15,8 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php
 	echo '<ul class="test">';
 	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-		var_dump($cart_item);
-		// echo '<li>' . $cart_item->data->post_name . '</li>';
+		echo '<li>' . $cart_item->data->post->post_name . '</li>';
 	}
 	echo '</ul>';
 ?>
