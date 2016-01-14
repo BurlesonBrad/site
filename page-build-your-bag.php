cd672045eb591447acba1f53f5c71c1d72485eaf
hyzershop <danny@hyzershop.com>
2016-01-14 00:55:33 -0500

    dsaf

--- page-build-your-bag.php
+++ page-build-your-bag.php
@@ -81,7 +81,7 @@ get_header(); ?>
 							}
 						}
 
-						if ( isset($byb) ) {
+						if ( isJson($byb) ) {
 							if ( is_string($byb) ) {
 								$byb_json = stripslashes($byb);
 								$byb_array = json_decode( $byb_json, true );
