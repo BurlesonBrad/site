5fdcd9930a46137faa7589b338ea64508986ba72
hyzershop <danny@hyzershop.com>
2016-01-14 03:00:50 -0500

    sadf

--- byb/byb.js
+++ byb/byb.js
@@ -178,7 +178,7 @@ promise_bags.success(function(data) {
 	$bagsMenu.append("<option value='My Bag'>My Bag</option>");
 });
 
-var $addToBagBtn = $("<button class='add-to-bag'><img class='not-yet-added' src='add-to-bag-icon.png' alt='Add' /><img class='added' src='/wp-content/themes/storefront-child/images/check-green.gif' alt='Add' />Add<span class='added'>ed</span> to bag<span class='added'>!</span></button>");
+var $addToBagBtn = $("<button class='add-to-bag'><img class='not-yet-added' src='/wp-content/themes/storefront-child/images/plus-pink.gif' alt='Add' /><img class='added' src='/wp-content/themes/storefront-child/images/check-green.gif' alt='Add' />Add<span class='added'>ed</span> to bag<span class='added'>!</span></button>");
 
 /***					***/
 /***	ADD TO BAG 		***/
