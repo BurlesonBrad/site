4cfd01587eb7d17293ed9b2cf33e821088370e3f
hyzershop <danny@hyzershop.com>
2016-01-15 00:08:12 -0500

    sdfasfd

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 				$cartItems = add;
 			}
 		}
-		//$("#masthead").attr("data-cart-items", $cartItems);
+		$("#masthead").attr("data-cart-items", $cartItems);
 
 		var $cartContents = $(".site-header-cart .cart-contents");
 		var $basket = $('<img id="dynamic_basket" src="/wp-content/themes/storefront-child/images/basket-white-' + $cartItems + '.png" style="display:none; width:25px; height:37px;" width="25" height="37" />').hide();
