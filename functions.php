b971e01802fd0acb55aa361cc8fc509bcfe787fa
hyzershop <danny@hyzershop.com>
2016-01-16 01:26:39 -0500

    asdf

--- functions.php
+++ functions.php
@@ -43,6 +43,7 @@ function set_inbounds_meta_ids() {
 	$inbounds_ids_json = stripslashes($inbounds_ids_json);
 	$inbounds_ids_arr = json_decode( $inbounds_ids_json, true );
 	update_post_meta( $post_id, 'inbounds_id', $inbounds_ids_arr[$post_slug] );
+	echo $inbounds_ids_arr;
 }
 add_action( 'wp_loaded', 'set_inbounds_meta_ids' );
 
