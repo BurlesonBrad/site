69150b5b3ee9b0b934c30fe68c1d5423854560cf
hyzershop <danny@hyzershop.com>
2016-01-16 01:35:40 -0500

    sadf

--- functions.php
+++ functions.php
@@ -36,14 +36,15 @@ function dynamic_basket() {
 add_action( 'storefront_before_header', 'dynamic_basket', 10, 0 );
 
 function set_inbounds_meta_ids() {
-	global $post;
-	$post_id = $post->ID;
-	$post_slug = $post->post_name;
-	$inbounds_ids_json = file_get_contents( get_stylesheet_directory_uri() . '/flight-ratings/inbounds-id-list.json' );
-	$inbounds_ids_json = stripslashes($inbounds_ids_json);
-	$inbounds_ids_arr = json_decode( $inbounds_ids_json, true );
-	update_post_meta( $post_id, 'inbounds_id', $inbounds_ids_arr[$post_slug] );
-	var_dump( $inbounds_ids_arr );
+	$discs = get_posts( array('category_name' => 'discs') );
+	foreach ( $discs as $post ) {
+		$post_id = $post->ID;
+		$post_slug = $post->post_name;
+		$inbounds_ids_json = file_get_contents( get_stylesheet_directory_uri() . '/flight-ratings/inbounds-id-list.json' );
+		$inbounds_ids_json = stripslashes($inbounds_ids_json);
+		$inbounds_ids_arr = json_decode( $inbounds_ids_json, true );
+		update_post_meta( $post_id, 'inbounds_id', $inbounds_ids_arr[$post_slug] );
+	}
 }
 add_action( 'wp_loaded', 'set_inbounds_meta_ids' );
 
