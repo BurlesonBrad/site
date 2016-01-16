4a45907fd0a3c1bf5113b00473f0a1246ac0f0fd
hyzershop <danny@hyzershop.com>
2016-01-16 00:59:43 -0500

    sadf

--- functions.php
+++ functions.php
@@ -43,6 +43,7 @@ function set_inbounds_meta_ids() {
 		$post_id = get_the_ID();
 		$post_slug = $post->post_name;
 		$inbounds_ids_json = file_get_contents( get_stylesheet_directory_uri() . '/flight-ratings/inbounds-id-list.json' );
+		$inbounds_ids_json = stripslashes($inbounds_ids_json);
 		$inbounds_ids_arr = json_decode( $inbounds_ids_json );
 		update_post_meta( $post_id, 'inbounds_id', $inbounds_ids_arr[$post_slug] );
 	endwhile;
