e9a39a2039d555be2a87323fd3fb02e3e7435467
hyzershop <danny@hyzershop.com>
2016-01-18 13:28:03 -0500

    asdf

--- inc/structure/footer.php
+++ inc/structure/footer.php
@@ -56,6 +56,8 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 		?>
 		<div class="site-info">
 			<?php echo esc_html( apply_filters( 'storefront_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date( 'Y' ) ) ); ?>
+			
+			<?php echo "hello" ?>
 			<?php
 			//if ( apply_filters( 'storefront_credit_link', true ) ) {
 			?>
