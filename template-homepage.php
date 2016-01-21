8aed8177ede983fd04abc9871892e112e693b808
hyzershop <danny@hyzershop.com>
2016-01-21 01:57:46 -0500

    sdfasfad

--- template-homepage.php
+++ template-homepage.php
@@ -146,7 +146,7 @@ get_header(); ?>
 						<form action="//hyzershop.us10.list-manage.com/subscribe/post?u=c294e1f306a856df1d2ffaee5&amp;id=e402d02301" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						    <div id="mc_embed_signup_scroll">
 							<label for="mce-EMAIL"><img src="/wp-content/themes/storefront-child/images/mail-icon-white.png" alt="get cool stuff in your inbox"/></label>
-							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="get cool stuff in your inbox" required>
 						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c294e1f306a856df1d2ffaee5_e402d02301" tabindex="-1" value=""></div>
 						    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
