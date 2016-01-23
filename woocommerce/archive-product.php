0f0ac342eb651bfbcabb2d48de8b17b05d4fce2c
hyzershop <danny@hyzershop.com>
2016-01-23 18:31:28 -0500

    fdas

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -39,8 +39,7 @@ get_header( 'shop' ); ?>
 			 * @hooked woocommerce_product_archive_description - 10
 			 */
 			do_action( 'woocommerce_archive_description' );
-			echo ("hello");
-			woocommerce_product_archive_description();
+			woocommerce_taxonomy_archive_description();
 		?>
 
 		<?php if ( have_posts() ) : ?>
