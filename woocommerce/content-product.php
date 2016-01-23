b23f6cbd26e16fc8a5a37faa3d831f5b90f058d8
hyzershop <danny@hyzershop.com>
2016-01-23 17:26:08 -0500

    sdfa

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -62,7 +62,11 @@ $brand = $brand[0]->slug;
 			 * @hooked woocommerce_template_loop_product_thumbnail - 10
 			 */
 			do_action( 'woocommerce_before_shop_loop_item_title' );
+		?>
 
+		<h4><?php echo $brand[0]->name; ?></h4>
+
+		<?php
 			/**
 			 * woocommerce_shop_loop_item_title hook
 			 *
