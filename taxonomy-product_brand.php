56601658e772377aab355cc58fd03d262a88eca5
hyzershop <danny@hyzershop.com>
2016-01-23 19:30:29 -0500

    adfsafds

--- taxonomy-product_brand.php
+++ taxonomy-product_brand.php
@@ -145,17 +145,6 @@ get_header( 'shop' ); ?>
 			?>
 			</ul><!--/.products-->
 
-
-
-			<?php
-				/**
-				 * woocommerce_after_shop_loop hook
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop' );
-			?>
-
 		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
 
 			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
