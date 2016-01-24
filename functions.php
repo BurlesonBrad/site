06721801a3d6e43a0b8aabc6f1901d837574cbe6
hyzershop <danny@hyzershop.com>
2016-01-24 04:03:40 -0500

    sfadfsad

--- functions.php
+++ functions.php
@@ -105,59 +105,6 @@ function woo_remove_product_tabs( $tabs ) {
 // 	return $args;
 // }
 
-if ( ! function_exists( 'woocommerce_output_related_products' ) ) {
-
-	/**
-	 * Output the related products.
-	 *
-	 * @subpackage	Product
-	 */
-	function woocommerce_output_related_products() {
-
-		$args = array(
-			'posts_per_page' 	=> 5,
-			'columns' 			=> 5,
-			'orderby' 			=> 'rand'
-		);
-
-		woocommerce_related_products( apply_filters( 'woocommerce_output_related_products_args', $args ) );
-	}
-}
-
-if ( ! function_exists( 'woocommerce_related_products' ) ) {
-
-	/**
-	 * Output the related products.
-	 *
-	 * @param array Provided arguments
-	 * @param bool Columns argument for backwards compat
-	 * @param bool Order by argument for backwards compat
-	 */
-	function woocommerce_related_products( $args = array(), $columns = false, $orderby = false ) {
-		if ( ! is_array( $args ) ) {
-			_deprecated_argument( __FUNCTION__, '2.1', __( 'Use $args argument as an array instead. Deprecated argument will be removed in WC 2.2.', 'woocommerce' ) );
-
-			$argsvalue = $args;
-
-			$args = array(
-				'posts_per_page' => $argsvalue,
-				'columns'        => $columns,
-				'orderby'        => $orderby,
-			);
-		}
-
-		$defaults = array(
-			'posts_per_page' => 5,
-			'columns'        => 5,
-			'orderby'        => 'rand'
-		);
-
-		$args = wp_parse_args( $args, $defaults );
-
-		wc_get_template( 'single-product/related.php', $args );
-	}
-}
-
 
 
 // if ( is_page_template( 'taxonomy-product_brand.php' ) ) {
