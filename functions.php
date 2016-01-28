c859f19081c1ee3e393cf72169956621ef819dbc
hyzershop <danny@hyzershop.com>
2016-01-28 00:37:44 -0500

    dfsaasdf

--- functions.php
+++ functions.php
@@ -152,7 +152,6 @@ function woo_new_product_tab_content() {
 	$stability = '';
 	$disc_type = '';
 	$stability = get_the_terms($post->ID, 'pa_stability');
-	var_dump($stability);
 	$stability = '<li>Stability: ' . str_replace('s', '', $stability->name) . '</li>';
 	
 	$disc_type = get_the_terms($post->ID, 'disc-type');
