f63c4e33f9a2c473366216bf2fe4aa0c639f5ebb
hyzershop <danny@hyzershop.com>
2016-01-28 00:33:49 -0500

    dfaadfs

--- functions.php
+++ functions.php
@@ -152,7 +152,8 @@ function woo_new_product_tab_content() {
 	$stability = '';
 	$disc_type = '';
 	$stability = get_the_terms($post->ID, 'stability');
-	$stability = '<li>Type: ' . str_replace('s', '', $stability->name) . '</li>';
+	var_dump($stability);
+	$stability = '<li>Stability: ' . str_replace('s', '', $stability->name) . '</li>';
 	
 	$disc_type = get_the_terms($post->ID, 'disc-type');
 	if ( count($disc_type) > 0 ) {
