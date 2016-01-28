7c65cc14d561a95ff64210b4dae5b571f0f06b4b
hyzershop <danny@hyzershop.com>
2016-01-28 00:31:32 -0500

    dsafasdf

--- functions.php
+++ functions.php
@@ -151,9 +151,9 @@ function woo_new_product_tab_content() {
 	global $post;
 	$stability = '';
 	$disc_type = '';
-	if ( get_post_meta($post->ID, 'stability', true) ) {
+	//if ( get_post_meta($post->ID, 'stability', true) ) {
 		$stability = '<li>Stability: ' . get_post_meta($post->ID, 'stability', true) . '</li>';
-	}
+	//}
 	$disc_type = get_the_terms($post->ID, 'disc-type');
 	if ( count($disc_type) > 0 ) {
 		$disc_type = '<li>Type: ' . str_replace('s', '', $disc_type[0]->name) . '</li>';
