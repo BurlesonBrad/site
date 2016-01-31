d63ba4ba032ffbe0b092183c63667aeb8326d9fb
hyzershop <danny@hyzershop.com>
2016-01-31 14:40:30 -0500

    fdas

--- js/scripts.js
+++ js/scripts.js
@@ -195,6 +195,7 @@ $(document).ready(function() {
 			var sizeI = parseInt( text.css("font-size"), 10 );
 			var winWidth = $(window).width();
 			var c = (winWidth/1600) < 1 ? winWidth/1600 : 1;
+			console.log( sizeI * c );
 			var sizeF = (sizeI * c) > 52 ? (sizeI * c) : 52;
 			console.log(sizeF);
 			text.css({
