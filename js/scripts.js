4c13abe657ed1b2f34ec275f35109949d63158a4
hyzershop <danny@hyzershop.com>
2016-01-31 15:04:46 -0500

    dfas

--- js/scripts.js
+++ js/scripts.js
@@ -194,7 +194,7 @@ $(document).ready(function() {
 		var sizeI = parseInt( $t.css("font-size"), 10 );
 		function setSize(text) {
 			var winWidth = $(window).width();
-			var c = winWidth > 1000 && winWidth < 1600 ? winWidth/1280 : 1;
+			var c = winWidth > 960 && winWidth < 1600 ? winWidth/1600 : 1;
 			console.log(c);
 			var sizeF = (sizeI * c);
 			console.log(sizeF);
@@ -207,7 +207,7 @@ $(document).ready(function() {
 			setSize($t);
 		});
 	}
-	responsiveText(".metaslider .caption-wrap");
+//	responsiveText(".metaslider .caption-wrap");
 	$(window).load(function() {
 		responsiveText(".metaslider .caption-wrap");
 	});
