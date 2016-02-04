7e18ff40bbfa9f16d686aa9475f2ced4a29a9739
hyzershop <danny@hyzershop.com>
2016-02-03 22:17:48 -0500

    dsfa

--- js/scripts.js
+++ js/scripts.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
 						vertAlign();
 					});
 
-					responsiveText(".metaslider .caption-wrap", 4, 6.2, "em");
+					responsiveText(".metaslider .caption-wrap", 4.2, 6.8, "em");
 
 					return true;
 				} else {
