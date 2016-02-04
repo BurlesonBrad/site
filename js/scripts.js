84612e29a3114005e188da08d9386a63795b6b7e
hyzershop <danny@hyzershop.com>
2016-02-04 00:03:54 -0500

    dsfa

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
 
 					responsiveText(".metaslider .caption", 2, 3.3, "em", 960, 1600, "high");
 					responsiveText(".metaslider .caption", 1.2, 2, "em", 300, 959, "low");
-					responsiveText(".metaslider .caption button", 0.6, 0.3, "em", 300, 1600);
+					responsiveText(".metaslider .caption button", 0.6, 0.38, "em", 300, 1600);
 
 					return true;
 				} else {
