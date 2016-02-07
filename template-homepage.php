e40b8980828fa39821d54c10b5cd5281bec69c52
hyzershop <danny@hyzershop.com>
2016-02-07 13:20:52 -0500

    asdf

--- template-homepage.php
+++ template-homepage.php
@@ -145,20 +145,16 @@ get_header(); ?>
 					<li><a href="http://facebook.com/hyzershop" target="_blank"><img src="/wp-content/themes/storefront-child/images/facebook-circle-blue.png" alt="like us on Facebook" /></a></li>
 					<li><a href="http://twitter.com/hyzer_shop" target="_blank"><img src="/wp-content/themes/storefront-child/images/twitter-circle-blue.png" alt="follow us on Twitter" /></a></li>
 					<li id="mc_embed_signup" class="email-bar">
-
 						<!-- Begin MailChimp Signup Form -->
-						<div id="mc_embed_signup">
 						<form action="//hyzershop.us10.list-manage.com/subscribe/post?u=c294e1f306a856df1d2ffaee5&amp;id=216402b312" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						    <div id="mc_embed_signup_scroll">
-							<label for="mce-EMAIL">Subscribe to our mailing list</label>
-							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+							<label for="mce-EMAIL"><img src="/wp-content/themes/storefront-child/images/mail-icon-white.png" alt="get cool stuff in your inbox"/></label>
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="get cool stuff in your inbox" required>
 						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c294e1f306a856df1d2ffaee5_216402b312" tabindex="-1" value=""></div>
 						    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 						    </div>
 						</form>
-						</div>
-
 						<!--End mc_embed_signup-->
 					</li>
 				</ul>
