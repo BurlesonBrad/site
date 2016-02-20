a6c3e264c21af7b4a7108c09389b19c923f49a44
hyzershop <danny@hyzershop.com>
2016-02-20 16:04:37 -0500

    sdfa

--- js/scripts.js
+++ js/scripts.js
@@ -198,7 +198,7 @@ $(document).ready(function() {
 			}, delay);
 		});
 
-		var $flightChart = $("<div class='flight-chart'><img src='http://ths09.inboundsdiscgolf.com/" + $product.data('inbounds-id') + ".png' alt='Inbounds flight chart for " + $product.data('product-slug') + "' />");
+		var $flightChart = $("<div class='flight-chart'><a href='http://inboundsdiscgolf.com'><img src='http://ths09.inboundsdiscgolf.com/" + $product.data('inbounds-id') + ".png' alt='Inbounds flight chart for " + $product.data('product-slug') + "' /></a>");
 		$longDesc.prepend($flightChart);
 	}
 	singleProductFlightData();
