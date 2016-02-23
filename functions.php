e04120b1e45fe1137dc871b6825acd06474e2322
hyzershop <danny@hyzershop.com>
2016-02-23 00:52:23 -0500

    sfda

--- functions.php
+++ functions.php
@@ -213,11 +213,11 @@ function setStockToOne() {
     foreach ( $variations as $product ) {
     	$product_id = $product->ID;
     	$new_stock_level = 1;
-    	$p = wc_get_product( $product_id );
-    	var_dump( $p );
+    	// $p = wc_get_product( $product_id );
+    	// var_dump( $p );
 
-		if ( ( ! metadata_exists( 'post', $product_id, '_stock' ) || $p->get_stock_quantity() !== $new_stock_level ) ) {
-			$p->set_stock( $new_stock_level );
+		if ( ( ! metadata_exists( 'post', $product_id, '_stock' ) || $product->get_stock_quantity() !== $new_stock_level ) ) {
+			$product->set_stock( $new_stock_level );
 		}
     }	 
 }
